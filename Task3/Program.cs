eb4f2affc2b48cd555c851cdf49fd943c107ebef
Valeria28-10 <valeriakonovalenko828@gmail.com>
2023-04-25 19:54:05 +0300

    ДЗ_1 Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = Number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
